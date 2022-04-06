916e04a91aeae15401432686ae61e555a89b1451
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-06 14:53:11 +0100

    add mobile nav(not complete) and change media query from max-width to min-width

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 import { carousel } from "./carousel.js";
+
+const mobileNav = document.querySelector(".mobile__nav");
+const closeBtn = document.querySelector(".mobile__nav--close-btn");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  mobileNav.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileNav.close();
+});
