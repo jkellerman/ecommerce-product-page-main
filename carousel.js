0396e8743588238ee05883364ce4829084144b60
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-06 11:08:09 +0100

    add functionality for carousel nav images when they are clicked and when the next and previous buttons are clicked

--- carousel.js
+++ carousel.js
@@ -18,16 +18,40 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
   targetSlide.classList.add("current-slide");
 };
 
+const updateNavImgs = (currentImg, targetImg) => {
+  currentImg.classList.remove("current-slide");
+  targetImg.classList.add("current-slide");
+};
+
 // when click left move slides to left
 prevButton.addEventListener("click", () => {
   const currentSlide = track.querySelector(".current-slide");
   const prevSlide = currentSlide.previousElementSibling;
+  const currentNavImage = carouselNav.querySelector(".current-slide");
+  const prevNavImage = currentNavImage.previousElementSibling;
   moveToSlide(track, currentSlide, prevSlide);
+  updateNavImgs(currentNavImage, prevNavImage);
 });
 // when click right move slides to right
 nextButton.addEventListener("click", () => {
   const currentSlide = track.querySelector(".current-slide");
   const nextSlide = currentSlide.nextElementSibling;
+  const currentNavImage = carouselNav.querySelector(".current-slide");
+  const nextNavImage = currentNavImage.nextElementSibling;
   moveToSlide(track, currentSlide, nextSlide);
+  updateNavImgs(currentNavImage, nextNavImage);
 });
 // when click nav indicators move to that slide
+
+carouselNav.addEventListener("click", (e) => {
+  // what indicator was clicked on
+  const targetImage = e.target.closest("button");
+  if (!targetImage) return;
+
+  const currentSlide = track.querySelector(".current-slide");
+  const currentNavImage = carouselNav.querySelector(".current-slide");
+  const targetIndex = carouselNavImgs.findIndex((img) => img === targetImage);
+  const targetSlide = slides[targetIndex];
+  moveToSlide(track, currentSlide, targetSlide);
+  updateNavImgs(currentNavImage, targetImage);
+});
