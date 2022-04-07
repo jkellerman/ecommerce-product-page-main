409a742492267c1d4505ff29f7583c53c5c7d262
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-07 09:57:11 +0100

    add toggle for cart icon, add empty cart text and hide items container

--- shoppingcart.js
+++ shoppingcart.js
@@ -3,25 +3,25 @@ const plus = document.querySelector(".plus");
 const amount = document.querySelector(".amount");
 const addToCartBtn = document.querySelector(".addtocart__btn");
 const cartBubble = document.querySelector(".cart__bubble");
+const cart = document.querySelector(".cart");
+const cartWindow = document.querySelector(".cart__window");
 
+// add amount with 10 as max
 plus.addEventListener("click", () => {
-  // add amount with 10 as max
   if (amount.innerText === "10") return;
   if (amount >= "0") {
     amount.innerText++;
   }
 });
-
+// minus amount
 minus.addEventListener("click", () => {
-  // minus amount
   if (amount.innerText === "0") return;
   if (amount > "0") {
     amount.innerText--;
   }
 });
-
+// add amount to cart
 addToCartBtn.addEventListener("click", () => {
-  // add amount to cart
   let cartTotal = cartBubble;
   let currentAmount = amount.innerText;
   if (currentAmount > "0") {
@@ -31,4 +31,10 @@ addToCartBtn.addEventListener("click", () => {
     cartTotal.innerText = "0";
     cartBubble.style.display = "none";
   }
+});
+
+// open cart window when cart is clicked
+
+cart.addEventListener("click", () => {
+  cartWindow.classList.toggle("hide__cart");
 });
